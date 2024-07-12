5525587f8ced9b8f40901943a2cf454e83aae289
Camel-app <camel.app@proton.me>
2024-07-12 15:58:57 +0200

    adapt world cafe

--- Online Study Files/main study/post_CAM/js/studyComp2.js
+++ Online Study Files/main study/post_CAM/js/studyComp2.js
@@ -162,6 +162,8 @@ const study = new lab.flow.Sequence({
     //new lab.plugins.Download()
   ],
   content: [
+    explanationSR1_htmlForm,
+    explanationSR2_htmlForm,
     break500,
     CAMfeedbackGeneral_htmlForm,
     explanationSR1_htmlForm,
